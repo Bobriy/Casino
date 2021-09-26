0455325328e8f1006d2ffab6e417a1e861ce0fd5
Alexey Arsentyev <90987055+Bobriy@users.noreply.github.com>
2021-09-26 22:18:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Casino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cash = 1000;
+
+            while (cash > 0)
+            {
+                Console.WriteLine("Select game \nr - Roulette \nb - Blackjack \ne - Exit the casino (Coming soon)" +
+                " \n\nCash: {0} \n", cash);
+
+                switch (Console.ReadLine())
+                {
+                    case "r":
+                        cash = Roulette.Play(cash);
+                        break;
+                    case "b":
+                        cash = Blackjack.Play(cash);
+                        break;
+                    case "e":
+                        Console.WriteLine("\nCOMING SOON");
+                        break;
+                    default:
+                        Console.WriteLine();
+                        break;
+                }
+            }            
+            Console.WriteLine("We are waiting for you again!");
+        }
+    }
+}
