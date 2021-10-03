8fd86ef1480f9b50c36cb92e046477ddce8eb274
Alexey Arsentyev <90987055+Bobriy@users.noreply.github.com>
2021-10-03 18:24:51 +0300

    Add files via upload

---
+++ Additional.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casino
+{
+    static class Additional
+    {
+        public static int Bet(int cash)
+        {
+            Console.WriteLine("\nBet value:");
+
+            string betinput = Console.ReadLine();
+
+            if (betinput == "e")
+            {
+                Console.WriteLine("\nExit\n");
+                return -1;
+            }
+            if (!int.TryParse(betinput, out int bet))
+            {
+                return 0;
+            }                      
+            if (cash < bet || bet <= 0)
+            {
+                return 0;
+            }
+            return bet;
+        }
+
+        public static int Defeat()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nYou lose!");
+            return 0;
+        }
+
+        public static int Victory()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nYou won!");
+            return 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Casino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int cash = 1000;
+
+            while (cash > 0)
+            {
+                Console.WriteLine("Select game \nr - Roulette \nb - Blackjack \ne - Exit the casino" +
+                " \n\nCash: {0} \n", cash);
+
+                switch (Console.ReadLine())
+                {
+                    case "r":
+                        cash = Roulette.Play(cash);
+                        break;
+                    case "b":
+                        cash = Blackjack.Play(cash);
+                        break;
+                    case "e":
+                        End(cash);
+                        break;                       
+                    default:
+                        Console.WriteLine();
+                        break;
+                }
+            }
+            Console.WriteLine("LOH!");
+        }
+
+        public static int Record { get; set; }
+        static void End(int cash)
+        {
+            using (var sw = new StreamWriter("record.txt", true)){}
+            
+            using (var sr = new StreamReader("record.txt"))
+            {
+                if (int.TryParse(sr.ReadLine(), out int record))
+                {
+                    Record = record;
+                }
+            }
+
+            using (var sw = new StreamWriter("record.txt"))
+            {
+                if (cash > Record)
+                {
+                    sw.Flush();
+                    sw.WriteLine(cash);
+                    Console.WriteLine("New record!");
+                }
+                else
+                {
+                    sw.WriteLine(Record);
+                    Console.WriteLine($"\nRecord: {Record}");
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\nRestart\n");
+            Console.ResetColor();
+        }
+    }
+}
